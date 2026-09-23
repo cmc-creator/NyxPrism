1a1da222ba77aea05c164c74ad2f7c90388a7fef
Connie Michelle <cmc@conniemichelleconsulting.com>
2026-09-23 16:14:32 -0700

    replace web app logo with refined prism asset

--- docs/sw.js
+++ docs/sw.js
@@ -3,7 +3,6 @@ const CACHE = 'nyx-v5';
 const SHELL = [
   '/manifest.json',
   '/nyx-brand.css?v=2',
-  '/nyx-brand.js?v=2',
   '/prism-icon-192.png',
   '/prism-icon-512.png',
   '/prism-icon.svg'
