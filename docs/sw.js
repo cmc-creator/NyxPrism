6fb96f5295470a60ae5aad10a11d647b93c65456
Connie Michelle <cmc@conniemichelleconsulting.com>
2026-09-23 16:42:47 -0700

    use supplied prism logo asset across web app

--- docs/sw.js
+++ docs/sw.js
@@ -3,9 +3,7 @@ const CACHE = 'nyx-v5';
 const SHELL = [
   '/manifest.json',
   '/nyx-brand.css?v=2',
-  '/prism-icon-192.png',
-  '/prism-icon-512.png',
-  '/prism-icon.svg'
+  '/prismlogo.jpg?v=1'
 ];
 const SENSITIVE_HTML = new Set(['/admin.html', '/dashboard.html', '/distribution.html', '/login.html', '/sign-request.html']);
 
